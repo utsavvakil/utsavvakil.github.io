e9fa24dca87b1b1ab8ca821fd7e81f08bdc40947
Utsav Vakil <utsavvakil@Utsavs-MacBook-Pro-2.local>
2016-09-25 13:53:49 -0400

    OnClick added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 function myFunction() {
-    document.getElementById("imagecaption").innerHTML = "Hello World";
+	if(document.getElementById("imagecaption")=="Net ID: uuv2")
+	{
+		document.getElementById("imagecaption").innerHTML = "Hello World";
+	}
+	else
+	{
+		document.getElementById("imagecaption").innerHTML = "Net ID: uuv2";
+	}
+    
 }
