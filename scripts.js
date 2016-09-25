d02c68365a917e440b82a825165108b6d103766c
Utsav Vakil <utsavvakil@Utsavs-MacBook-Pro-2.local>
2016-09-25 14:40:56 -0400

    Added Modal

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-function myFunction() {
+var modal = document.getElementById('myModal');
 
-	var id = document.getElementById("imagecaption");
-	if(document.getElementById("imagecaption").innerHTML=="Net ID: uuv2")
-	{
-		document.getElementById("imagecaption").innerHTML = "Hello World";
-	}
-	else
-	{
-		document.getElementById("imagecaption").innerHTML = "Net ID: uuv2";
-	}
-    
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
